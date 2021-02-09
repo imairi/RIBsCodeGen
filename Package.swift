3454605139e9807582014b73b9ca81e9bf98bf21
imairi <imairi@gmail.com>
2021-02-09 10:43:36 +0900

    update swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
@@ -7,14 +7,17 @@ let package = Package(
         .executable(name: "ribscodegen", targets: ["RIBsCodeGen"])
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.22.0"),
-        .package(url: "https://github.com/kylef/PathKit", from: "0.9.2"),
-        .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0")
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.31.0"),
+        .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "3.2.0")
     ],
     targets: [
         .target(
             name: "RIBsCodeGen", 
-            dependencies: ["SourceKittenFramework", "PathKit", "Rainbow"]
+            dependencies: [
+                .product(name: "SourceKittenFramework", package: "SourceKitten"),
+                "PathKit",
+                "Rainbow"]
         )
     ]
 )
