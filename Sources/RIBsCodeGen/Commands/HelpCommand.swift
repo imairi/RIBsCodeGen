fb2fdc97dd0f93f23ffdf6ce4a7248756b775a79
imairi <imairi@gmail.com>
2021-02-09 16:35:14 +0900

    fix Help command

--- Sources/RIBsCodeGen/Commands/HelpCommand.swift
+++ Sources/RIBsCodeGen/Commands/HelpCommand.swift
@@ -9,7 +9,28 @@ import Foundation
 
 struct HelpCommand: Command {
     func run() -> Result {
-        let helpMessage = "USAGE: ribscodegen [analyze target path] [--under [RIB name]] "
+        let helpMessage = """
+        USAGE: ribscodegen [action] [arguments]
+
+        - bulk generation
+        ribscodegen scaffold [tree.md] --parent [parent RIB name]
+
+        - add RIB
+        ribscodegen add [target RIB name]
+
+        - add viewless RIB
+        ribscodegen add [target RIB name] --noview
+
+        - add RIB and link parent
+        ribscodegen add [target RIB name] --parent [parent RIB name]
+
+        - link RIB
+        ribscodegen link [target RIB name] --parent [parent RIB name]
+
+        cf. More details in https://github.com/imairi/RIBsCodeGen/
+
+        """.lightBlue
+
         return .success(message: helpMessage)
     }
 }
