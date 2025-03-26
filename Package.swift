ecc63407577226702c9900cdc30db03b4bbf3c5a
imairi <imairi@gmail.com>
2025-03-26 14:38:57 +0900

    update SourceKitten version and RIBsCodegen minimum OS version

--- Package.swift
+++ Package.swift
@@ -3,11 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "RIBsCodeGen",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
         .executable(name: "ribscodegen", targets: ["RIBsCodeGen"])
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.31.0"),
+        .package(url: "https://github.com/jpsim/SourceKitten", from: "0.37.0"),
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.2.0")
     ],
